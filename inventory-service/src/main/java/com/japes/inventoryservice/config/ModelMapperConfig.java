8200df4059e9777f74bb31ec142c9a81be841746
Shubham Paranjape <shubhamparanjape@Shubhams-MacBook-Pro.local>
2026-07-19 13:00:37 +0530

    feat: configure ModelMapper bean

---
+++ inventory-service/src/main/java/com/japes/inventoryservice/config/ModelMapperConfig.java
@@ -0,0 +1,18 @@
+package com.japes.inventoryservice.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+	
+	@Bean
+	public ModelMapper modelMapper() {
+		ModelMapper mapper = new ModelMapper();
+		mapper.getConfiguration()
+			.setSkipNullEnabled(true)
+			.setFieldMatchingEnabled(true);
+		return mapper;
+	}
+}
