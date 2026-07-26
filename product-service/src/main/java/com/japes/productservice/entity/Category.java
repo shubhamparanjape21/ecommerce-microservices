c08e7fc1d5d98e16ce67f0cfc240b46d64b6d8c1
Shubham Paranjape <shubhamparanjape@Shubhams-MacBook-Pro.local>
2026-07-26 16:35:54 +0530

    feat: add Category entity

---
+++ product-service/src/main/java/com/japes/productservice/entity/Category.java
@@ -0,0 +1,25 @@
+package com.japes.productservice.entity;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "categories")
+public class Category {
+	@Column(nullable = false, unique = true)
+	private String name;
+	
+	@Column(length = 1000)
+	private String description;
+	
+	@OneToMany(mappedBy = "category")
+	private List<Product> products;
+}
