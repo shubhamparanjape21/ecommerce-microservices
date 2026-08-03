85d1e1aaaad4fcceb03915d957e1fa401a83825a
Shubham Paranjape <shubhamparanjape21@gmail.com>
2026-08-03 19:19:32 +0530

    feat(order): configure JPA auditing with AuditorAware implementation

---
+++ order-service/src/main/java/com/japes/orderservice/config/AuditorAwareImpl.java
@@ -0,0 +1,16 @@
+package com.japes.orderservice.config;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+@Component("auditorAware")
+public class AuditorAwareImpl implements AuditorAware<String> {
+
+	@Override
+	public Optional<String> getCurrentAuditor() {
+		return Optional.of(System.getProperty("user.name"));
+	}
+
+}
