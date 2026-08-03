d0500a3ae1f4db61b4f052bf702831498ec91b51
Shubham Paranjape <shubhamparanjape21@gmail.com>
2026-08-03 19:26:21 +0530

    refactor(order,inventory): remove unused ModelMapper properties

--- order-service/src/main/java/com/japes/orderservice/config/ModelMapperConfig.java
+++ order-service/src/main/java/com/japes/orderservice/config/ModelMapperConfig.java
@@ -9,10 +9,6 @@ public class ModelMapperConfig {
 	
 	@Bean
 	public ModelMapper modelMapper() {
-		ModelMapper mapper = new ModelMapper();
-		mapper.getConfiguration()
-			.setSkipNullEnabled(true) // skip null values during mapping
-			.setFieldMatchingEnabled(true);
-		return mapper;
+		return new ModelMapper();
 	}
 }
