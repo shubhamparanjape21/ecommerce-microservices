e7d63041c4d1420d799cdebb7f6e3880fea269af
Shubham Paranjape <shubhamparanjape21@gmail.com>
2026-08-04 19:35:30 +0530

    feat(order-service): register service with Eureka Discovery Server

--- order-service/src/main/java/com/japes/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/japes/orderservice/OrderServiceApplication.java
@@ -2,10 +2,12 @@ package com.japes.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
+@EnableDiscoveryClient
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
