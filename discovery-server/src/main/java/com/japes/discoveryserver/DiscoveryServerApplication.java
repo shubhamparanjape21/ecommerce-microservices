9784f3125da2778f03435434db5e44a369d5d12e
Shubham Paranjape <shubhamparanjape21@gmail.com>
2026-08-04 19:01:08 +0530

    feat: add Eureka Discovery Server project

---
+++ discovery-server/src/main/java/com/japes/discoveryserver/DiscoveryServerApplication.java
@@ -0,0 +1,20 @@
+package com.japes.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootApplication
+@EnableEurekaServer
+@Slf4j
+public class DiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		log.debug("Start of main(-) in Eureka Server App");
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+		log.debug("End of main(-) in Eureka Server App");
+	}
+
+}
