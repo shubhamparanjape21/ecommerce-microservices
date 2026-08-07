b08a846ca894748df55273ed8c273aae2ca45d04
Shubham Paranjape <shubhamparanjape21@gmail.com>
2026-08-07 21:24:59 +0530

    feat: create Payment entity

---
+++ payment-service/src/main/java/com/japes/paymentservice/entity/Payment.java
@@ -0,0 +1,39 @@
+package com.japes.paymentservice.entity;
+
+import java.math.BigDecimal;
+
+import com.japes.paymentservice.enums.PaymentMethod;
+import com.japes.paymentservice.enums.PaymentStatus;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "payments")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Payment extends BaseModel {
+	@Column(nullable = false, unique = true, length = 30)
+	private String paymentReference;
+	@Column(nullable = false, length = 30)
+	private String orderNumber;
+	@Column(nullable = false, precision = 10, scale = 2)
+	private BigDecimal amount;
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false, length = 20)
+	private PaymentMethod paymentMethod;
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false, length = 20)
+	private PaymentStatus paymentStatus;
+	@Column(unique = true, length = 30)
+	private String transactionId;
+}
