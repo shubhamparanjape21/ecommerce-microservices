5c193355d83bfd77df77fa4f186cd9be361b1c57
Shubham Paranjape <shubhamparanjape21@gmail.com>
2026-08-08 22:05:03 +0530

    feat: configure Discovery and Config Clients

--- payment-service/src/main/java/com/japes/paymentservice/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/japes/paymentservice/PaymentServiceApplication.java
@@ -2,10 +2,12 @@ package com.japes.paymentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAware")
+@EnableDiscoveryClient
 public class PaymentServiceApplication {
 
 	public static void main(String[] args) {
