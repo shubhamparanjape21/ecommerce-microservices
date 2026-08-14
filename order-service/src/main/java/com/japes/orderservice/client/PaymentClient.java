91f43616a9ce34b9340d8871661766f2cbf4965b
Shubham Paranjape <shubhamparanjape21@gmail.com>
2026-08-15 00:07:38 +0530

    feat(order): add payment service feign client

---
+++ order-service/src/main/java/com/japes/orderservice/client/PaymentClient.java
@@ -0,0 +1,20 @@
+package com.japes.orderservice.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.japes.orderservice.dto.client.CreatePaymentRequest;
+import com.japes.orderservice.dto.client.PaymentInitiationResponse;
+import com.japes.orderservice.dto.client.PaymentResponse;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+@FeignClient(name = "payment-service")
+public interface PaymentClient {
+	@PostMapping("/api/v1/payments")
+    public PaymentResponse createPayment(@RequestBody CreatePaymentRequest request);
+
+    @PostMapping("/api/v1/payments/initiate/{paymentReference}")
+    public PaymentInitiationResponse initiatePayment(@PathVariable String paymentReference);
+}
