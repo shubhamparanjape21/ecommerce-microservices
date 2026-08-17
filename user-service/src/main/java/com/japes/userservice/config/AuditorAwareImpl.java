56204b3928044c1fae8feac26df3f9340041e334
Shubham Paranjape <shubhamparanjape21@gmail.com>
2026-08-17 19:31:19 +0530

    feat(User): configure JPA auditing

---
+++ user-service/src/main/java/com/japes/userservice/config/AuditorAwareImpl.java
@@ -0,0 +1,16 @@
+package com.japes.userservice.config;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+@Component("auditorAware")
+public class AuditorAwareImpl implements AuditorAware<String>{
+
+	@Override
+	public Optional<String> getCurrentAuditor() {
+		return Optional.of(System.getProperty("user.name"));
+	}
+
+}
