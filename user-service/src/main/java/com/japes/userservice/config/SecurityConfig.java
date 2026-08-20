bea83a67338891490e8db76c32b56c39c0323f88
Shubham Paranjape <shubhamparanjape21@gmail.com>
2026-08-20 20:29:32 +0530

    feat(user-service): configure jwt security filter chain

--- user-service/src/main/java/com/japes/userservice/config/SecurityConfig.java
+++ user-service/src/main/java/com/japes/userservice/config/SecurityConfig.java
@@ -3,12 +3,27 @@ package com.japes.userservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.japes.userservice.security.JwtAuthenticationFilter;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+	
+	private final JwtAuthenticationFilter jwtAuthenticationFilter;
+	
+	public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
+	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
@@ -18,13 +33,43 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.csrf(csrf -> csrf.disable())
+	        .sessionManagement(session ->
+	        	session.sessionCreationPolicy(
+	        			SessionCreationPolicy.STATELESS
+	        	)
+	        )
+	        
+	        .exceptionHandling(exception ->
+	        exception.authenticationEntryPoint(
+	            (request, response, authException) -> {
+	                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+	                response.setContentType("application/json");
+	                response.getWriter().write(
+	                    """
+	                    {
+	                        "status": 401,
+	                        "message": "Authentication required"
+	                    }
+	                    """
+	                );
+	            }
+	        )
+	    )
+	        
             .authorizeHttpRequests(request -> request
                 .requestMatchers(
-                    "/api/v1/users/**",
+                	"/api/v1/users/register",
+                    "/api/v1/users/login",
                     "/swagger-ui/**",
                     "/v3/api-docs/**"
                 ).permitAll()
+                
                 .anyRequest().authenticated()
+            )
+        
+        .addFilterBefore(
+                jwtAuthenticationFilter,
+                UsernamePasswordAuthenticationFilter.class
             );
 
         return http.build();
