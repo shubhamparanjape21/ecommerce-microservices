81133120c2e01855b6bde09bcc133b957e2094e1
Shubham Paranjape <shubhamparanjape21@gmail.com>
2026-08-27 23:20:11 +0530

    feat(inventory): propagate correlation id to logs

---
+++ inventory-service/src/main/java/com/japes/inventoryservice/filter/CorrelationIdFilter.java
@@ -0,0 +1,41 @@
+package com.japes.inventoryservice.filter;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class CorrelationIdFilter extends OncePerRequestFilter {
+
+	private static final String CORRELATION_ID = "X-Correlation-ID";
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		String correlationId = request.getHeader(CORRELATION_ID);
+
+        if (correlationId == null || correlationId.isBlank()) {
+            correlationId = UUID.randomUUID().toString();
+        }
+
+        MDC.put(CORRELATION_ID, correlationId);
+        System.out.println("PRODUCT FILTER - Correlation ID = " + correlationId);
+        response.setHeader(CORRELATION_ID, correlationId);
+
+        try {
+            filterChain.doFilter(request, response);
+        } finally {
+            MDC.remove(CORRELATION_ID);
+        }
+
+	}
+
+}
