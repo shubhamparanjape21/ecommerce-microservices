e8fafa853ff1db2711fd6d9d57f6d38434fd22a4
Shubham Paranjape <shubhamparanjape21@gmail.com>
2026-08-27 21:59:58 +0530

    feat(order): add request interceptor for user feign client

--- order-service/src/main/java/com/japes/orderservice/config/UserFeignConfig.java
+++ order-service/src/main/java/com/japes/orderservice/config/UserFeignConfig.java
@@ -1,11 +1,16 @@
 package com.japes.orderservice.config;
 
+import org.slf4j.MDC;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import com.japes.orderservice.exception.UserNotFoundException;
 
+import feign.RequestInterceptor;
 import feign.codec.ErrorDecoder;
+import jakarta.servlet.http.HttpServletRequest;
 
 @Configuration
 public class UserFeignConfig {
@@ -19,5 +24,43 @@ public class UserFeignConfig {
 			
 			return new RuntimeException("User service returned error: " + response.status());
 		};
+	}
+	
+	@Bean
+    public RequestInterceptor userRequestInterceptor() {
+
+        return requestTemplate -> {
+
+            // Forward Correlation ID
+            String correlationId = MDC.get("X-Correlation-ID");
+
+            if (correlationId != null && !correlationId.isBlank()) {
+                requestTemplate.header(
+                        "X-Correlation-ID",
+                        correlationId
+                );
+            }
+
+            // Forward JWT
+            ServletRequestAttributes attributes =
+                    (ServletRequestAttributes)
+                            RequestContextHolder.getRequestAttributes();
+
+            if (attributes != null) {
+
+                HttpServletRequest request =
+                        attributes.getRequest();
+
+                String authorization =
+                        request.getHeader("Authorization");
+
+                if (authorization != null && !authorization.isBlank()) {
+                    requestTemplate.header(
+                            "Authorization",
+                            authorization
+                    );
+                }
+            }
+        };
 	}
 }
