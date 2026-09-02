c815e3d1439848bb346e86a368464659a4d0412b
Shubham Paranjape <shubhamparanjape21@gmail.com>
2026-09-02 19:32:43 +0530

    feat: consume order created event in inventory service

---
+++ inventory-service/src/main/java/com/japes/inventoryservice/config/KafkaConsumerConfig.java
@@ -0,0 +1,69 @@
+package com.japes.inventoryservice.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JacksonJsonDeserializer;
+
+import com.japes.inventoryservice.event.OrderCreatedEvent;
+
+@Configuration
+public class KafkaConsumerConfig {
+
+    @Bean
+    public ConsumerFactory<String, OrderCreatedEvent> consumerFactory() {
+
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(
+            ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+            "localhost:9092"
+        );
+
+        config.put(
+            ConsumerConfig.GROUP_ID_CONFIG,
+            "inventory-service-v3"
+        );
+
+        config.put(
+            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+            StringDeserializer.class
+        );
+
+        config.put(
+            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+            JacksonJsonDeserializer.class
+        );
+
+        config.put(
+            "spring.json.value.default.type",
+            OrderCreatedEvent.class
+        );
+
+        config.put(
+            "spring.json.trusted.packages",
+            "com.japes.inventoryservice.event"
+        );
+
+        return new DefaultKafkaConsumerFactory<>(config);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, OrderCreatedEvent>
+            kafkaListenerContainerFactory() {
+
+        ConcurrentKafkaListenerContainerFactory<String, OrderCreatedEvent> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+
+        factory.setConsumerFactory(consumerFactory());
+
+        return factory;
+    }
+}
