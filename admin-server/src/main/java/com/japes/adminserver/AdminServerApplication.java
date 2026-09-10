a2586b31a383910586b16a9f1e0cd4e24a2a34c7
Shubham Paranjape <shubhamparanjape21@gmail.com>
2026-09-10 16:52:23 +0530

    fix: enable Spring Boot Admin Server

--- admin-server/src/main/java/com/japes/adminserver/AdminServerApplication.java
+++ admin-server/src/main/java/com/japes/adminserver/AdminServerApplication.java
@@ -3,7 +3,10 @@ package com.japes.adminserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
 @SpringBootApplication
+@EnableAdminServer
 public class AdminServerApplication {
 
 	public static void main(String[] args) {
